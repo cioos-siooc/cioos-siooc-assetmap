ce335753b76a22f16ef7d7cb3b5b08d187ac5be6
Joel <joel.friesen@hakaimagazine.com>
2019-11-21 15:12:51 -0800

    Init

--- asset/js/asset_ckan.js
+++ asset/js/asset_ckan.js
@@ -958,14 +958,14 @@ function generateDetailsPanel( dataset ) //, language, dataset_id, title, descri
     }
     ret_html += '<div class="asset-actions">';
     ret_html += '<span class="details_label">Information:</span>';
-    ret_html += '<a data-toggle="collapse" href="#' + dataset["id"] + '_collapse' + '" role="button" onclick="showDatasetDetailDescription(\'' + dataset["id"] + '\');">' + i18nStrings.getUIString("details") + '</a>';
+    ret_html += '<a data-toggle="collapse" href="#' + dataset["id"] + '_collapse' + '" role="button" onclick="showDatasetDetailDescription(\'' + dataset["id"] + '\')">' + i18nStrings.getUIString("details") + '</a>';
     
     // check if geomeetry details available for this dataset
     if ( datasetHasSpatial(dataset) )
     {
-        ret_html += '<a href="#" onclick="showInGeometryLayer(\'' + dataset["id"] + '\');">' + i18nStrings.getUIString("map") + '</a> ';
+        ret_html += '<a href="#" onclick="showInGeometryLayer(\'' + dataset["id"] + '\');");">' + i18nStrings.getUIString("map") + '</a> ';
     }
-    // ret_html += '<button type="button" class="button" onclick="selectAndCenterFeatureOnMap(\'' + dataset["id"] + '\');">Map</button> ';
+    // ret_html += '<button type="button" class="button" onclick="selectFeatureOnMap(\'' + dataset["id"] + '\');");">Map</button> ';
     // ret_html += '<a class="button" data-toggle="collapse" href="#' + dataset["id"] + '_collapse' + '" role="button">details</a>';
     ret_html += '</div>';
     ret_html += '<div class="collapse" id="' + dataset["id"] + '_collapse' + '">';
@@ -1165,9 +1165,7 @@ function clearAllDatasets()
     document.getElementById('dataset_desc').innerHTML = "";
 
     // clear map display
-    if (clusterLayer !== undefined){
-        clusterLayer.setVisible(false);   
-    }
+    clusterLayer.setVisible(false);
     vectorLayer.setVisible(false);
     let vectorSource= vectorLayer.getSource();
     vectorSource.clear();
@@ -1247,44 +1245,31 @@ function updateDatasetDetails( datasets )
 function updateDatasetDetailsFromCache( datasetid )
 {
     let element = ckan_server.datasetDetails[datasetid];
-    // update and open panel
+    // update and open panel 
     let itemid = '#' + element['id'] + '_collapse';
-    if (document.getElementById(element['id'] + '_collapse')) {
-        document.getElementById(element['id'] + '_collapse').innerHTML = generateCompleteDetailsPanel(element);
-    }
+    document.getElementById(element['id'] + '_collapse').innerHTML = generateCompleteDetailsPanel(element);
     $(itemid).collapse("show");
 }
 
-function showDatasetDetailDescription( datasetid , goto_description = true, select_point = true, center_point = true)
+function showDatasetDetailDescription( datasetid )
 /**
  * Shows Dataset Detail Description and adjusts the scroll so that detail panel is visible
- * @param  {[string]} datasetid
- * @param  {[bool]} goto_description - scroll to the current description in the right panel if True
- * @param  {[bool]} select_point - selects corresponding feature (point) on the map
- * @param  {[bool]} center_point - center corresponding feature (point) on the map
+ * @param  {[string]} datasetid [element]
  */
 {
     // collapse other detail panels
     $('#dataset_desc').find('.collapse').each(function() {
-        if ($(this).attr('id') != datasetid) {
+        if ($(this).attr('id') != datasetid || true) {
             $(this).collapse('hide');
         }
     });
-    // load details panel
+    // show details panel
     callDatasetDetailDescription(datasetid);
-    // scroll up to this panel
-    if (goto_description) {
-        $("#"+datasetid).on("shown.bs.collapse", function() {
-            let topPos = $('#dataset_desc').scrollTop() + $("#"+datasetid).position().top;
-            $('#dataset_desc').animate({scrollTop:topPos}, 500);
-        });
-    }
-    if (select_point) {
-        selectFeatureOnMap(datasetid);
-    }
-    if (center_point) {
-        centerFeatureOnMap(datasetid);
-    }
+    // sroll up to this panel
+    $("#"+datasetid).on("shown.bs.collapse", function() {
+        let topPos = $('#dataset_desc').scrollTop() + $("#"+datasetid).position().top;
+        $('#dataset_desc').animate({scrollTop:topPos}, 500);
+    });
 }
 
 function callDatasetDetailDescription( datasetid )
